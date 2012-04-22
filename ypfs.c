de628922250ca9fe98cc7eae267c8065f0662703
Dylan Garrett <dylan.garrett@gmail.com>
2012-04-22 14:45:00 -0700

    Can write now. File just gets copied

--- ypfs.c
+++ ypfs.c
@@ -19,14 +19,17 @@
 static const char *hello_str = "Hello World!\n";
 static const char *hello_path = "/hello";
 
+//FILE *log_file;
+//struct timeval time;
+
 void mylog(char* log_str)
 {
 	FILE *log_file;
 	struct timeval time;
 	gettimeofday(&time, NULL);
 	
-	log_file = fopen("/tmp/fuse_log.txt", "a");
-	fprintf(log_file, "%ld:%ld :: %s", time.tv_sec, time.tv_usec, log_str);
+	log_file = fopen("/tmp/ypfs.log", "a");
+	fprintf(log_file, "%ld:%ld :: %s\n", time.tv_sec, time.tv_usec, log_str);
 	fclose(log_file);
 }
 
@@ -54,6 +57,11 @@ NODE init_node(const char* name, NODE_TYPE type)
 	temp->type = type;
 	temp->children = NULL;
 	temp->num_children = 0;
+
+	if (type == NODE_FILE) {
+		temp->hash = malloc(sizeof(char) * 100);
+		sprintf(temp->hash, "%d", rand() % 100000);
+	}
 
 	return temp;
 }
@@ -102,25 +110,45 @@ void remove_child(NODE parent, NODE child)
 	free(child);
 }
 
-/*
- * split - a pointer to an array of strings
- */
-int split_path(const char* path, char*** split)
+int elements_in_path(const char* path)
 {
 	char* curr = (char*)path;
-	int slash_count = 0;
+	int elements = 0;
 	while (curr++) {
 		if (*curr == '/')
-			slash_count++;
+			elements++;
 	}
 	// if there's a trailing slash
 	if (curr[-1] == '/')
-		slash_count--;
+		elements--;
 
+	return elements;
+}
+
+/*
+ * split - a pointer to an array of strings
+ * returns number of strings split into
+ */
+int split_path(const char* path, char** split)
+{
+	char* curr = (char*)path;
+	int elements = elements_in_path(path);
+	int i = 0;
+	
 	// slash_count now has number of elements in path
-	*split = malloc(sizeof(char*) * slash_count);
+	//*split = malloc(sizeof(char*) * slash_count);
+	
+	for (i = 0; i < elements; i++) {
+		int n = 0;
+		if (*curr == '/')
+			curr++;
+		split[i] = malloc(sizeof(char) * (strlen(path) + 1));
+		while(*curr != '/' && *curr != '\0') {
+			split[i][n++] = *(curr++);
+		}
+	}
 
-	return 0;
+	return elements;
 }
 
 void to_full_path(const char* path, char* full)
@@ -128,23 +156,69 @@ void to_full_path(const char* path, char* full)
 	sprintf(full, "/home/dylangarrett/.ypfs/%s", path);
 }
 
-NODE node_for_path(char* path)
+
+NODE _node_for_path(char* path, NODE curr)
 {
+	/*int elements = elements_in_path(path);
+	char** split = malloc(sizeof(char*) * elements);
+	NODE curr = root;
+	int i = 0;
+	elements = split_path(path, split);
+	for (i = 0; i < elements; i++) {
+		
+	}*/
+
+	char name[1000];
+	int i = 0;
+
+	//mylog(path);
+
+	if (*path == '/')
+		path++;
+	
+	while(*path && *path != '/') {
+		name[i++] = *(path++);
+	}
+	name[i] = '\0';
+
+	//mylog("name:");
+	//mylog(name);
+
+	if (i == 0) {
+		//mylog("node found:");
+		//mylog(curr->name);
+		return curr;
+	}
+
+	for (i = 0; i < curr->num_children; i++) {
+		//mylog("comparing name to :");
+		//mylog(curr->children[i]->name);
+		if (0 == strcmp(name, curr->children[i]->name))
+			return _node_for_path(path, curr->children[i]);
+	}
+
+	//mylog("node not found");
 	return NULL;
 }
+
+NODE node_for_path(const char* path) 
+{
+        return _node_for_path((char*)path, root);
+}
+
 
 
 // from example
 static int ypfs_getattr(const char *path, struct stat *stbuf)
 {
 	int res = 0;
-	mylog("getattr\n");
+	mylog("getattr");
 
 	memset(stbuf, 0, sizeof(struct stat));
 	if (strcmp(path, "/") == 0) {
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2;
-	} else if (1 || strcmp(path, hello_path) == 0) {
+	} else if (node_for_path(path) != NULL) {
 		stbuf->st_mode = S_IFREG | 0777;
 		stbuf->st_nlink = 1;
 		stbuf->st_size = strlen(hello_str);
@@ -160,7 +234,7 @@ static int ypfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	int i;
 	(void) offset;
 	(void) fi;
-	mylog("readdir\n");
+	mylog("readdir");
 	if (strcmp(path, "/") != 0)
 		return -ENOENT;
 
@@ -168,7 +242,7 @@ static int ypfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	filler(buf, "..", NULL, 0);
 	//filler(buf, hello_path + 1, NULL, 0);
 	for (i = 0; i < root->num_children; i++) {
-		filler(buf, root->children[i]->name + 1, NULL, 0);
+		filler(buf, root->children[i]->name, NULL, 0);
 	}
 
 	return 0;
@@ -177,13 +251,13 @@ static int ypfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 // from example
 static int ypfs_open(const char *path, struct fuse_file_info *fi)
 {
-	mylog("open\n");
-	return 0;
-	if (strcmp(path, hello_path) != 0)
+	mylog("open");
+
+	if (node_for_path(path) == NULL)
 		return -ENOENT;
 
-	if ((fi->flags & 3) != O_RDONLY)
-		return -EACCES;
+	//if ((fi->flags & 3) != O_RDONLY)
+	//	return -EACCES;
 
 	return 0;
 }
@@ -194,7 +268,7 @@ static int ypfs_read(const char *path, char *buf, size_t size, off_t offset,
 {
 	size_t len;
 	(void) fi;
-	mylog("read\n");
+	mylog("read");
 	return 0;
 	if(strcmp(path, hello_path) != 0)
 		return -ENOENT;
@@ -212,8 +286,13 @@ static int ypfs_read(const char *path, char *buf, size_t size, off_t offset,
 
 static int ypfs_create(const char *path, mode_t mode, struct fuse_file_info *fi)
 {
-	NODE new_node = init_node(path, NODE_FILE);
-	mylog("create\n");
+	NODE new_node;
+	char* end = (char*)path;
+	while(*end != '\0') end++;
+	while(*end != '/' && end >= path) end--;
+	if (*end == '/') end++;
+	new_node = init_node(end, NODE_FILE);
+	mylog("create");
 	add_child(root, new_node);
 	return 0;
 }
@@ -223,11 +302,11 @@ static int ypfs_write(const char *path, const char *buf, size_t size, off_t offs
 	FILE *new_file;
 	int fd;
 	int res;
-	char random_name[100];
 	char full_file_name[1000];
-	mylog("write\n");
-	sprintf(random_name, "%d", rand() % 100);
-	to_full_path(random_name, full_file_name);
+	NODE file_node = node_for_path(path);
+	mylog("write");
+	to_full_path(file_node->hash, full_file_name);
+	mylog(full_file_name);
 
 	/*new_file = fopen(full_file_name, "wb");
 
@@ -235,13 +314,17 @@ static int ypfs_write(const char *path, const char *buf, size_t size, off_t offs
 
 	fclose(new_file);*/
 
-	fd = open(full_file_name, O_WRONLY);
-	if(fd == -1)
+	fd = open(full_file_name, O_WRONLY | O_CREAT, 0666);
+	if(fd == -1) {
+		mylog("fd == -1");
 		return -errno;
+	}
 
 	res = pwrite(fd, buf, size, offset);
-	if(res == -1)
+	if(res == -1) {
+		mylog("pwrite error");
 		res = -errno;
+	}
 
 	close(fd);
 
@@ -250,14 +333,14 @@ static int ypfs_write(const char *path, const char *buf, size_t size, off_t offs
 
 static int ypfs_utimens(const char *path, const struct timespec tv[2])
 {
-	mylog("utimens\n");
-	add_child(root, init_node(path, NODE_FILE));
+	mylog("utimens");
+	//add_child(root, init_node(path, NODE_FILE));
 	return 0;
 }
 
 static int ypfs_mknod(const char *path, mode_t mode, dev_t device)
 {
-	mylog("mknod\n");
+	mylog("mknod");
 	return 0;
 }
 
@@ -274,8 +357,9 @@ static struct fuse_operations ypfs_oper = {
 
 int main(int argc, char *argv[])
 {
+	mylog("===========start============");
 	printf("mkdir: %d\n", mkdir("/home/dylangarrett/.ypfs", 0777));
 	root = init_node("/", NODE_DIR);
-	add_child(root, init_node("/test", NODE_FILE));
+	add_child(root, init_node("test", NODE_FILE));
 	return fuse_main(argc, argv, &ypfs_oper, NULL);
 }
